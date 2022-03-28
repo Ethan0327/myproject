e5bcfc31ba8d427c75d1d3439b0f9be18e583b8f
Ethan0327 <99637134+Ethan0327@users.noreply.github.com>
2022-03-28 15:07:47 +0800

    Create ex6.cpp

---
+++ ex6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Please enter a number:\n"<<endl;
+    cin>>x;
+    if(x<0){
+            cout<<"y=1"<<endl;
+    }
+    else if(x=0){
+            cout<<"y=5"<<endl;
+    }
+    else
+        cout<<"y=3"<<endl;
+    return 0;
+}
